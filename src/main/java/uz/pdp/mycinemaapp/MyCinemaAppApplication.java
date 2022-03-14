b80dcbe1137bb3dd1ef455b62930cd904032e5d6
AladdinRukhiddinov <aladinrukhiddinov@gmail.com>
2022-03-14 14:49:57 +0500

    create project

---
+++ src/main/java/uz/pdp/mycinemaapp/MyCinemaAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.mycinemaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCinemaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/mycinemaapp/MyCinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.mycinemaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
